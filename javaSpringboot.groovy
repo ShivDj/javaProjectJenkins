6b9efe1a4e5e57ff5742c2fce0d0e5ab358e6ece
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-26 12:20:00 +0530

    Update javaSpringboot.groovy

--- javaSpringboot.groovy
+++ javaSpringboot.groovy
@@ -10,7 +10,7 @@ pipeline {
         stage('Build') {
             steps {
                 // Get some code from a GitHub repository
-                git 'https://github.com/ShivDj/javaProjectJenkins.git'
+                git 'https://github.com/ShivDj/javaProjectJenkins.git' 
 
                 // Run Maven on a Unix agent.
                 sh "mvn -Dmaven.test.failure.ignore=true clean package"
@@ -19,12 +19,10 @@ pipeline {
         stage('transfer') {
             steps {
                 
-               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/target/SpringBootDemoProject-0.0.1-SNAPSHOT.jar ubuntu@13.127.89.96:/home/ubuntu/'
+               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/target/SpringBootDemoProject-0.0.1-SNAPSHOT.jar ubuntu@13.232.2.165:/home/ubuntu/java'
+               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/target/Dockerfile ubuntu@13.232.2.165:/home/ubuntu/java'
+               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/target/docker-compose.yml ubuntu@13.232.2.165:/home/ubuntu/java'
                
-               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/javachatapp.service ubuntu@13.127.89.96:/home/ubuntu/'
-                      
-               sh 'rsync -r -e  "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/start.sh ubuntu@13.127.89.96:/home/ubuntu/'
-               sh 'ssh -i /var/lib/jenkins/public_instance_key.pem ubuntu@13.127.89.96 "bash /home/ubuntu/start.sh"'        
                 
             }
         }
