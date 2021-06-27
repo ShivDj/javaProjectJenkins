c683cf105b52e77ca711be59cd850f5b0923a05d
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-27 21:10:36 +0530

    Update javaSpringboot.groovy

--- javaSpringboot.groovy
+++ javaSpringboot.groovy
@@ -7,7 +7,7 @@ pipeline {
     }
 
     stages {
-        stage('Build') {
+        stage('BUILDING_STAGE') {
             steps {
                 // Get some code from a GitHub repository
                 git 'https://github.com/ShivDj/javaProjectJenkins.git' 
@@ -16,16 +16,20 @@ pipeline {
                 sh "mvn -Dmaven.test.failure.ignore=true clean package"
             }
         } 
-        stage('transfer') {
+        stage('MOVING_STAGE') {
             steps {
                 
                sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/target/SpringBootDemoProject-0.0.1-SNAPSHOT.jar ubuntu@13.127.44.149:/home/ubuntu/java'
                sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/Dockerfile ubuntu@13.127.44.149:/home/ubuntu/java'
                sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/docker-compose.yml ubuntu@13.127.44.149:/home/ubuntu/java'
-               
-                
+               sh 'rsync -r -e "ssh -i /var/lib/jenkins/public_instance_key.pem" /var/lib/jenkins/workspace/javaspring/Remove.sh ubuntu@13.127.44.149:/home/ubuntu/java'               
             }
         }
-        
+        stage('EXECUTING_STAGE') {
+            steps {
+                sh 'ssh -i /var/lib/jenkins/public_instance_key.pem ubuntu@13.127.44.149 "bash /home/ubuntu/java/Remove.sh"'              
+            }
+        }
+
     }
 }
