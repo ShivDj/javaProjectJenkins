c0416e84cc3bde9fc74c4496de22f9f39d06703c
ShivDj <62321737+ShivDj@users.noreply.github.com>
2021-06-28 10:04:42 +0530

    Update Remove.sh

--- Remove.sh
+++ Remove.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-sudo docker rm -f java_container
-sudo docker rmi -f java_image
+
+if [ /home/ubuntu/java_image ]; then
+   sudo docker rmi -f java_image
+fi
+
+if [ /home/ubuntu/java_container ]; then
+   sudo docker rmi -f java_container
+fi
+
+#sudo docker rm -f java_container
+#sudo docker rmi -f java_image
 
 cd /home/ubuntu/java
 sudo docker-compose up --build -d
